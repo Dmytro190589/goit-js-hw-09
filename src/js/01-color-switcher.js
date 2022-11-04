09e6764b1f50c84b3db34cb1958b24b1ca92f73a
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-11-04 09:54:50 +0200

    task 9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+'use script'
+const backgColor = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+startBtn.addEventListener('click', () => {
+    backgColor.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(
+    () => (bodyColor.style.backgroundColor = getRandomHexColor()),
+    1000 );
+    startBtn.disabled = true;
+     stopBtn.disabled = false;
+});
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  clearInterval(timerId);
+});
+
 
