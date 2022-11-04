d57c77482138089379e9dd40a824290d6f497478
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-11-04 19:35:09 +0200

    task9-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,39 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setInterval(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay });
+        // Reject
+      }
+    },delay);
+  })
+
 }
+const formHandler = (el)=>{
+  el.preventDefault();
+  let {target}= el;
+let delay = +(target.elements.delay.value);
+const step = +(target.elements.step.value);
+const amount = +(target.elements.amount.value);
+for (let i = 0; i < amount; i++){
+  createPromise( i,delay)
+    .then(({position,delay})=>{
+      Notiflix.Notify.success(` Fulfilled promise ${position} in ${delay}ms`)
+    })
+    .catch(({position,delay})=>{
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`)
+    });
+    delay += step;
+  
+}
+}
+form.addEventListener('submit',formHandler);
+
+
